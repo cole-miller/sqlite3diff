1c4c9becaacd900b660e82c191c170f7bfa7603d
Cole Miller <m@cole-miller.net>
2024-01-11 15:09:02 -0500

    Switch to u32 for page numbers

--- src/bin/delta.rs
+++ src/bin/delta.rs
@@ -1,7 +1,7 @@
+use clap::Parser;
 use sqlite3diff::*;
 use std::fs::File;
 use std::path::PathBuf;
-use clap::Parser;
 
 #[derive(Parser)]
 struct Args {
@@ -9,13 +9,13 @@ struct Args {
     delta_file: PathBuf,
     /// Number of checksums in the follower's signature.
     #[arg(long, short = 'f')]
-    follower_checksum_count: u64,
+    follower_checksum_count: u32,
     /// Number of matching checksums between the follower and the leader.
     #[arg(long, short = 'm')]
-    matching_count: u64,
+    matching_count: u32,
     /// Number of pages on the leader with cached checksums that don't match with the follower.
     #[arg(long, short = 's')]
-    leader_addl_cached_count: u64,
+    leader_addl_cached_count: u32,
 }
 
 fn main() {
@@ -23,7 +23,7 @@ fn main() {
     let db = File::open(args.db_file).unwrap();
     let db_size = db.metadata().unwrap().len();
     assert!(db_size % PAGE_SIZE as u64 == 0);
-    let page_count = db_size / PAGE_SIZE as u64;
+    let page_count = (db_size / PAGE_SIZE as u64) as u32;
     assert!(args.matching_count <= args.follower_checksum_count);
     assert!(args.matching_count + args.leader_addl_cached_count <= page_count);
     let lookup = build_lookup(args.follower_checksum_count);
@@ -35,8 +35,10 @@ fn main() {
         &lookup,
     );
     let expected_delta_pages = page_count - args.matching_count;
-    let expected_delta_bytes = expected_delta_pages * PAGE_SIZE as u64;
-    println!("Expected delta size: about {expected_delta_pages} pages, {expected_delta_bytes} bytes");
+    let expected_delta_bytes = expected_delta_pages as u64 * PAGE_SIZE as u64;
+    println!(
+        "Expected delta size: about {expected_delta_pages} pages, {expected_delta_bytes} bytes"
+    );
     let mut delta = File::create(args.delta_file).unwrap();
     let begin = std::time::Instant::now();
     stream_delta(&db, cksums, &lookup, &mut delta).unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,9 @@ use std::os::fd::*;
 pub const PAGE_SIZE: usize = 4096;
 pub const CKSUM_SIZE: usize = 32;
 
-fn sendfile(
-    output: BorrowedFd<'_>,
-    input: &File,
-    start: u64,
-    len: u64,
-) -> Result<(), std::io::Error> {
+pub type PageNumber = u32;
+
+pub fn sendfile(output: RawFd, input: &File, start: u64, len: u64) -> Result<(), std::io::Error> {
     let mut off: libc::off_t = start.try_into().unwrap();
     let mut count: libc::size_t = len.try_into().unwrap();
     while count > 0 {
@@ -36,24 +33,24 @@ impl std::hash::Hash for Cksum {
 
 impl nohash::IsEnabled for Cksum {}
 
-pub type Lookup = indexmap::IndexMap<Cksum, u64, nohash::BuildNoHashHasher<Cksum>>;
+pub type Lookup = indexmap::IndexMap<Cksum, u32, nohash::BuildNoHashHasher<Cksum>>;
 
 #[derive(Clone, Copy, Debug)]
 pub enum Del {
-    Reuse { start: u64, len: u64 },
-    Transfer { start: u64, len: u64 },
+    Reuse { start: u32, len: u32 },
+    Transfer { start: u32, len: u32 },
 }
 
 #[derive(Debug)]
 pub struct Delta {
-    pos: u64,
-    wip: Del,
-    pages_transferred: u64,
-    pages_reused: u64,
+    pub pos: u32,
+    pub wip: Del,
+    pub pages_transferred: u32,
+    pub pages_reused: u32,
 }
 
 impl Delta {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             pos: 0,
             wip: Del::Reuse { start: 0, len: 0 },
@@ -62,56 +59,65 @@ impl Delta {
         }
     }
 
-    fn feed(&mut self, off: Option<u64>) -> Option<Del> {
+    pub fn feed(&mut self, off: Option<u32>) -> Option<Del> {
         let del = match (self.wip, off) {
             (Del::Reuse { start, ref mut len }, Some(off)) if start + *len == off => {
-                *len += PAGE_SIZE as u64;
+                *len += 1;
                 self.pages_reused += 1;
                 None
             }
             (Del::Transfer { ref mut len, .. }, None) => {
-                *len += PAGE_SIZE as u64;
+                *len += 1;
                 self.pages_transferred += 1;
                 None
             }
             (del, Some(off)) => {
-                self.wip = Del::Reuse {
-                    start: off,
-                    len: PAGE_SIZE as u64,
-                };
+                self.wip = Del::Reuse { start: off, len: 1 };
                 self.pages_reused += 1;
                 Some(del)
             }
             (reuse @ Del::Reuse { len, .. }, None) => {
                 self.wip = Del::Transfer {
                     start: self.pos,
-                    len: PAGE_SIZE as u64,
+                    len: 1,
                 };
                 self.pages_transferred += 1;
                 (len > 0).then(|| reuse)
             }
         };
-        self.pos += PAGE_SIZE as u64;
+        self.pos += 1;
         del
     }
 }
 
-fn stream_del(del: Del, input: &File, output: &mut (impl Write + AsFd)) -> Result<(), std::io::Error> {
+pub fn write_del(
+    del: Del,
+    input: &File,
+    output: &mut (impl Write + AsRawFd),
+) -> Result<(), std::io::Error> {
     match del {
         Del::Transfer { start, len } => {
-                // TODO TCP_CORK?
-                let mut hdr = [0; 9];
-                hdr[0] = 0x44;
-                hdr[1..].copy_from_slice(&u64::to_be_bytes(len));
-                output.write_all(&hdr)?;
-                sendfile(output.as_fd(), input, start, len)?;
+            // TODO TCP_CORK?
+            let (start, len) = (
+                start as u64 * PAGE_SIZE as u64,
+                len as u64 * PAGE_SIZE as u64,
+            );
+            let mut hdr = [0; 9];
+            hdr[0] = 0x44;
+            hdr[1..].copy_from_slice(&u64::to_be_bytes(len));
+            output.write_all(&hdr)?;
+            sendfile(output.as_raw_fd(), input, start, len)?;
         }
         Del::Reuse { start, len } => {
-                let mut hdr = [0; 17];
-                hdr[0] = 0x54;
-                hdr[1..9].copy_from_slice(&u64::to_be_bytes(start));
-                hdr[9..].copy_from_slice(&u64::to_be_bytes(len));
-                output.write_all(&hdr)?;
+            let (start, len) = (
+                start as u64 * PAGE_SIZE as u64,
+                len as u64 * PAGE_SIZE as u64,
+            );
+            let mut hdr = [0; 17];
+            hdr[0] = 0x54;
+            hdr[1..9].copy_from_slice(&u64::to_be_bytes(start));
+            hdr[9..].copy_from_slice(&u64::to_be_bytes(len));
+            output.write_all(&hdr)?;
         }
     }
     Ok(())
@@ -121,7 +127,7 @@ pub fn stream_delta(
     input: &File,
     cksums: impl Iterator<Item = Option<Cksum>>,
     lookup: &Lookup,
-    output: &mut (impl Write + AsFd),
+    output: &mut (impl Write + AsRawFd),
 ) -> Result<(), std::io::Error> {
     let mut delta = Delta::new();
     let (mut hit_count, mut miss_count, mut unavailable_count) = (0, 0, 0);
@@ -131,25 +137,23 @@ pub fn stream_delta(
                 unavailable_count += 1;
                 None
             }
-            Some(cksum) => {
-                match lookup.get(&cksum) {
-                    None => {
-                        miss_count += 1;
-                        None
-                    }
-                    Some(&off) => {
-                        hit_count += 1;
-                        Some(off)
-                    }
+            Some(cksum) => match lookup.get(&cksum) {
+                None => {
+                    miss_count += 1;
+                    None
                 }
-            }
+                Some(&off) => {
+                    hit_count += 1;
+                    Some(off)
+                }
+            },
         };
         if let Some(del) = delta.feed(off) {
-            stream_del(del, input, output)?;
+            write_del(del, input, output)?;
         }
     }
     // flush
-    stream_del(delta.wip, input, output)?;
+    write_del(delta.wip, input, output)?;
     output.write_all(&[0])?;
     dbg!(hit_count);
     dbg!(miss_count);
@@ -158,11 +162,9 @@ pub fn stream_delta(
     Ok(())
 }
 
-pub fn build_lookup(n: u64) -> Lookup {
+pub fn build_lookup(n: u32) -> Lookup {
     let mut rng = SmallRng::from_entropy();
-    (0..n)
-        .map(|i| (Cksum(rng.gen()), i * PAGE_SIZE as u64))
-        .collect()
+    (0..n).map(|i| (Cksum(rng.gen()), i)).collect()
 }
 
 struct Lcg {
@@ -173,7 +175,7 @@ impl RngCore for Lcg {
     fn next_u32(&mut self) -> u32 {
         (self.next_u64() >> 32) as u32
     }
-    
+
     fn next_u64(&mut self) -> u64 {
         self.state *= 0x7c3c3267d015ceb5;
         self.state += 0x24bd2d95276253a9;
@@ -197,15 +199,15 @@ impl RngCore for Lcg {
 }
 
 pub fn make_leader_checksums(
-    num_pages: u64,
-    matching_count: u64,
-    non_matching_count: u64,
+    num_pages: u32,
+    matching_count: u32,
+    non_matching_count: u32,
     lookup: &Lookup,
 ) -> impl '_ + Iterator<Item = Option<Cksum>> {
     struct MakeChecksums<'a> {
-        matching_count: u64,
-        non_matching_count: u64,
-        non_cached_count: u64,
+        matching_count: u32,
+        non_matching_count: u32,
+        non_cached_count: u32,
         lookup: &'a Lookup,
         rng: Lcg,
     }
@@ -221,8 +223,14 @@ pub fn make_leader_checksums(
             let mut non_matching = [0; CKSUM_SIZE];
             non_matching[..8].copy_from_slice(&u64::to_be_bytes(x)[..]);
             let non_matching = Cksum(non_matching);
-            let mut pairs = [(Some(matching), &mut self.matching_count), (Some(non_matching), &mut self.non_matching_count), (None, &mut self.non_cached_count)];
-            let pair = pairs.choose_weighted_mut(&mut self.rng, |it| *it.1).unwrap();
+            let mut pairs = [
+                (Some(matching), &mut self.matching_count),
+                (Some(non_matching), &mut self.non_matching_count),
+                (None, &mut self.non_cached_count),
+            ];
+            let pair = pairs
+                .choose_weighted_mut(&mut self.rng, |it| *it.1)
+                .unwrap();
             *pair.1 -= 1;
             Some(pair.0)
         }
@@ -230,10 +238,11 @@ pub fn make_leader_checksums(
 
     let non_cached_count = num_pages - matching_count - non_matching_count;
     (MakeChecksums {
-        matching_count: dbg!(matching_count),
-        non_matching_count: dbg!(non_matching_count),
-        non_cached_count: dbg!(non_cached_count),
+        matching_count,
+        non_matching_count,
+        non_cached_count,
         lookup,
         rng: Lcg { state: 17 },
-    }).take(num_pages as usize)
+    })
+    .take(num_pages as usize)
 }
